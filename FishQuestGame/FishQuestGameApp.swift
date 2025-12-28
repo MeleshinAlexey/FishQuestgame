faf3efa8feddef2928fd5cce1598e5191d9e7af5
Meleshin Alexey <meleshinalexey5@gmail.com>
2025-12-28 19:21:56 +0300

    Initial Commit

---
+++ FishQuestGame/FishQuestGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishQuestGameApp.swift
+//  FishQuestGame
+//
+//  Created by Alexey Meleshin on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FishQuestGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
